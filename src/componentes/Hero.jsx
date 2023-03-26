f508d9136e854990ad5e8da28f8de49bd2b787d3
catharygr <103951447+catharygr@users.noreply.github.com>
2023-03-26 17:49:52 +0100

    Trabajando en Hero css

--- src/componentes/Hero.jsx
+++ src/componentes/Hero.jsx
@@ -1,5 +1,23 @@
 import React from "react";
 
 export default function Hero() {
-  return <p>Hero</p>;
+  return (
+    <div className="hero-container">
+      <div className="hero-grid">
+        <div className="hero-fondo"></div>
+        <div className="hero-text">
+          <h2>Las mejores guías gps de aventura alrededor de la ciudad de:</h2>
+          <h2>Sarajevo</h2>
+        </div>
+        <div className="hero-select">
+          <h3 className="color-white">Elige tu pasión</h3>
+          <div className="hero-btn-container">
+            <button className="btn">Esquiar</button>
+            <button className="btn">MTB</button>
+            <button className="btn">Caminar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
