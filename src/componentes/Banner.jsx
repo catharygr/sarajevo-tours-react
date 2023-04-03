c6df8569fd18f8978ece4bfec10ab83f37486229
catharygr <103951447+catharygr@users.noreply.github.com>
2023-04-03 15:19:06 +0100

    Trabajando en componente Banner

--- src/componentes/Banner.jsx
+++ src/componentes/Banner.jsx
@@ -1,10 +1,25 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 
 export default function Banner({ bannerType }) {
+  const { color, title, description, imgUrl, productoId } = bannerType;
+
   return (
     <div className="banner-container">
-      <aside>
-        <h1>banner</h1>
+      <aside
+        className="banner-aside"
+        style={{ backgroundColor: `hsl(var(--color-${color}))` }}
+      >
+        <div className="color-white">
+          <h4>{title}</h4>
+          <p>{description}</p>
+        </div>
+        <div
+          className="btn-img-container"
+          style={{ backgroundImage: `url(${imgUrl})` }}
+        >
+          <button className="btn btn-green">Comprar</button>
+        </div>
       </aside>
     </div>
   );
